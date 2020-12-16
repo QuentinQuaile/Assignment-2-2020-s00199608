c9b9be771ec667a3dfe97e9a6326ef99fc2d2e94
Quentin Quaile <S00199608@mail.itsligo.ie>
2020-12-16 11:50:16 +0000

    Employee Class

---
+++ Assignment 2 2020 s00199608/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2_2020_s00199608
+{
+    public abstract class Employee
+    {
+        string fName { get; set; }
+        string lName { get; set; }
+            
+        public abstract double CalculateMonthlyPay(decimal Money); 
+    }
+    public class FullTime : Employee
+    {
+        decimal salary { get; set; }
+    }
+    public class PartTime : Employee
+    {
+        decimal hourlyRate { get; set; }
+        double hoursWorked { get; set; }
+    }
+}
