b052c9907f19723963e9a3baf8e55af1b88e0047
Quentin Quaile <S00199608@mail.itsligo.ie>
2020-12-16 17:46:01 +0000

    Employees listbox

--- Assignment 2 2020 s00199608/Employee.cs
+++ Assignment 2 2020 s00199608/Employee.cs
@@ -8,30 +8,71 @@ namespace Assignment_2_2020_s00199608
 {
     public abstract class Employee
     {
-        string fName { get; set; }
-        string lName { get; set; }
-            
+        public string fName { get; set; }
+        public string lName { get; set; }
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(string first, string last)
+        {
+            fName = first;
+            lName = last;
+
+        }
+
+
         public abstract decimal CalculateMonthlyPay(); 
     }
     public class FullTime : Employee
     {
-        public decimal salary { get; set; }
+        public decimal Salary { get; set; }
+
+        public FullTime()
+        {
+
+        }
+
+        public FullTime(string first, string last, decimal salary) : base(first,last)
+        {
+            Salary = salary;
+        }
 
         override public decimal CalculateMonthlyPay()
         {
-            decimal monthlyPay;
-            monthlyPay = salary / 12;
-            return monthlyPay;
+            return (decimal)(Salary / 12);
+        }
+        public override string ToString()
+        {
+            return fName + ", " + lName + "- Full Time";
         }
     }
     public class PartTime : Employee
     {
-        decimal hourlyRate { get; set; }
-        double hoursWorked { get; set; }
+        public decimal HourlyRate { get; set; }
+        public double HoursWorked { get; set; }
 
+        public PartTime()
+        {
+            
+        }
+
+        public PartTime(string first, string last, decimal hourlyRate, double hoursWorked): base(first, last)
+        {
+            HourlyRate = hourlyRate;
+
+            HoursWorked = hoursWorked;
+        }
+
+        public override string ToString()
+        {
+            return fName + ", " + lName + "- Part Time";
+        }
         override public decimal CalculateMonthlyPay()
         {
-            return (decimal)((double)hourlyRate * hoursWorked);
+            return (decimal)((double)HourlyRate * HoursWorked);
         }
     }
 }
--- Assignment 2 2020 s00199608/MainWindow.xaml.cs
+++ Assignment 2 2020 s00199608/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 namespace Assignment_2_2020_s00199608
 {
@@ -20,9 +21,32 @@ namespace Assignment_2_2020_s00199608
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Employee> first = new ObservableCollection<Employee>();
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FullTime employee = new FullTime("Jess", "WALSH", 200);
+            FullTime employee1 = new FullTime("Joe", "MURPHY", 300);
+            PartTime employee2 = new PartTime("Jane", "JONES", 15, 10);
+            PartTime employee3 = new PartTime("John", "SMITH", 20, 7);
+
+
+            first.Add(employee);
+            first.Add(employee1);
+            first.Add(employee2);
+            first.Add(employee3);
+
+            listBox.ItemsSource = first;
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
